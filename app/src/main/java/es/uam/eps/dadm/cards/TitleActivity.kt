407f038526bcb972eb0500012bbf6467fcf92109
franccy.sambrano <franccy.sambrano@estudiante.uam.es>
2021-03-22 23:20:37 +0100

    Version arreglada de locos xD

--- app/src/main/java/es/uam/eps/dadm/cards/TitleActivity.kt
+++ app/src/main/java/es/uam/eps/dadm/cards/TitleActivity.kt
@@ -4,15 +4,16 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.TextView
+import androidx.databinding.DataBindingUtil
+import es.uam.eps.dadm.cards.databinding.ActivityTitleBinding
 
 class TitleActivity : AppCompatActivity() {
+    lateinit var binding: ActivityTitleBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_title)
-        supportActionBar?.hide()
 
-        findViewById<TextView>(R.id.cards_title_text_view).setOnClickListener {
-            startActivity(Intent(this, MainActivity::class.java))
-        }
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_title)
+        supportActionBar?.hide()
     }
 }
