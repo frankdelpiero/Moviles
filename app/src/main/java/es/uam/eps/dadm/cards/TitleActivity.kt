635f307873944c981f786038910d520bc90aad49
franccy.sambrano <franccy.sambrano@estudiante.uam.es>
2021-03-24 19:18:00 +0100

    FinishActivity 18

--- app/src/main/java/es/uam/eps/dadm/cards/TitleActivity.kt
+++ app/src/main/java/es/uam/eps/dadm/cards/TitleActivity.kt
@@ -8,14 +8,34 @@ import androidx.databinding.DataBindingUtil
 import es.uam.eps.dadm.cards.databinding.ActivityTitleBinding
 import timber.log.Timber
 
-class TitleActivity : AppCompatActivity() {
+class TitleActivity : AppCompatActivity(),TitleFragment.onTitleFragmentInteractionListener {
     lateinit var binding: ActivityTitleBinding
+
+    override fun onStudy() {
+        var fragment = StudyFragment()
+
+        supportFragmentManager
+                ?.beginTransaction()
+                ?.replace(R.id.fragment_container, fragment)
+                .addToBackStack(null)
+                ?.commit()
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
         binding = DataBindingUtil.setContentView(this, R.layout.activity_title)
-        Timber.i("ENTRA NO")
         supportActionBar?.hide()
+        var fragment = supportFragmentManager //Lista de fragmentos
+                .findFragmentById(R.id.fragment_container)
+
+        if (fragment == null){ //En caso de que sea la primera vez que registramos el fragmento
+            fragment = TitleFragment()
+
+            supportFragmentManager
+                    .beginTransaction()
+                    .add(R.id.fragment_container, fragment)
+                    .commit()
+        }
     }
 }
--- app/src/main/java/es/uam/eps/dadm/cards/TitleFragment.kt
+++ app/src/main/java/es/uam/eps/dadm/cards/TitleFragment.kt
@@ -1,5 +1,6 @@
 package es.uam.eps.dadm.cards
 
+import android.content.Context
 import android.content.Intent
 import android.os.Bundle
 import androidx.fragment.app.Fragment
@@ -22,6 +23,23 @@ private const val ARG_PARAM2 = "param2"
  * create an instance of this fragment.
  */
 class TitleFragment : Fragment() {
+
+    interface onTitleFragmentInteractionListener {
+        fun onStudy()
+    }
+    var listener: onTitleFragmentInteractionListener? = null // Asigno a listener el fragmetnos a solicitar
+
+
+    override fun onAttach(context: Context) {
+        super.onAttach(context)
+        listener = context as onTitleFragmentInteractionListener?
+    }
+
+    override fun onDetach() {
+        super.onDetach()
+        listener = null
+    }
+
     // TODO: Rename and change types of parameters
     private var param1: String? = null
     private var param2: String? = null
@@ -46,7 +64,8 @@ class TitleFragment : Fragment() {
             false)
         binding.cardsTitleTextView.setOnClickListener {
             Timber.i("AYUDDDAAA")
-            startActivity(Intent(activity, StudyActivity::class.java))
+            //startActivity(Intent(activity, StudyActivity::class.java))
+            listener?.onStudy()
         }
 
         return binding.root
